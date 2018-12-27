3c450737d63141ec953bd355a2fc1ca4f6e2cd98
Jovi <nean@protonmail.com>
2018-12-26 21:33:43 -0300

    finished css and logics

--- index.rb
+++ index.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'fileutils'
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 get '/' do
@@ -6,8 +7,15 @@ get '/' do
 end
 
 post '/download' do
+   list = Dir.glob("./files/*.*").map{|f| f.split('/').last}
+   if list.size >= 30
+      FileUtils.rm_f Dir.glob("./files/*")
+   end
    system("./youtube-dl/youtube-dl -x --audio-format mp3 -f 'best[filesize<80M]' #{params['link']} -o 'files/%(title)s.%(ext)s'")
    list = Dir.glob("./files/*.*").map{|f| f.split('/').last}
-   send_file "./files/#{list[-1]}", :filename => list[-1], :type => 'Application/octet-stream'
-   "Hello"
+   sorted_list = list.sort_by {|filename| File.ctime("./files/#{filename}") }
+   send_file "./files/#{sorted_list.last}", :filename => sorted_list.last, :type => 'Application/octet-stream'
+   
+   redirect '/'
+  
 end
